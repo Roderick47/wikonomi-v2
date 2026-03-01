2326e673ff46a29334eec2c73196821cc375faea
Roderick_S <rodericksasu2@gmail.com>
2026-03-01 22:21:51 +1000

    Fix PORT handling in start script

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 cd wikonomi
 python manage.py migrate
 python manage.py collectstatic --noinput
+# Use Render's PORT or default to 10000
+export PORT=${PORT:-10000}
 gunicorn wikonomi.wsgi:application --bind 0.0.0.0:$PORT
