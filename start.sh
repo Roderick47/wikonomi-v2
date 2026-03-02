3f822693b805ccfa9b38babdcd185b6225afd2f1
Roderick_S <rodericksasu2@gmail.com>
2026-03-02 22:04:15 +1000

    Create all required media directories for the app

--- start.sh
+++ start.sh
@@ -2,9 +2,15 @@
 cd wikonomi
 python manage.py migrate
 python manage.py collectstatic --noinput
-# Create media directory if it doesn't exist
+# Create all media directories if they don't exist
 mkdir -p /var/data/media/profile_pics
+mkdir -p /var/data/media/product_images
+mkdir -p /var/data/media/business_images
+mkdir -p /var/data/media/price_report_images
 # Set correct permissions
 chmod 755 /var/data/media
 chmod 755 /var/data/media/profile_pics
+chmod 755 /var/data/media/product_images
+chmod 755 /var/data/media/business_images
+chmod 755 /var/data/media/price_report_images
 gunicorn wikonomi.wsgi:application --bind 0.0.0.0:10000
