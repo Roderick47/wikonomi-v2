1290075c5c16b1c2e3c2b8ccbef62c92a04ee61f
Roderick_S <rodericksasu2@gmail.com>
2026-03-02 21:26:37 +1000

    Configure media files for persistent disk storage

--- start.sh
+++ start.sh
@@ -2,4 +2,7 @@
 cd wikonomi
 python manage.py migrate
 python manage.py collectstatic --noinput
+# Create media directory if it doesn't exist
+mkdir -p /var/data/media
+mkdir -p /var/data/media/profile_pics
 gunicorn wikonomi.wsgi:application --bind 0.0.0.0:10000
--- wikonomi/wikonomi/settings.py
+++ wikonomi/wikonomi/settings.py
@@ -161,7 +161,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (for profile pictures)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/data/media' if os.environ.get('RENDER') else os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
