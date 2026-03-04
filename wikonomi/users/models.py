22ce4fb46e76c8b8dd37cd07274ab25011e22697
Roderick_S <rodericksasu2@gmail.com>
2026-03-04 18:06:41 +1000

    Fix Profile model to use ResizedImageField for R2 storage

--- wikonomi/users/models.py
+++ wikonomi/users/models.py
@@ -2,36 +2,22 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
-from PIL import Image
-import os
+from django_resized import ResizedImageField
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(upload_to='profile_pics/', default='profile_pics/default.jpg')
+    profile_picture = ResizedImageField(
+        upload_to='profile_pics/', 
+        default='profile_pics/default.jpg',
+        null=True, 
+        blank=True,
+        size=[300, 300], 
+        quality=85, 
+        force_format='JPEG'
+    )
     
     def __str__(self):
         return f'{self.user.username} Profile'
-    
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        
-        # Optimize profile picture
-        if self.profile_picture and hasattr(self.profile_picture, 'path') and os.path.exists(self.profile_picture.path):
-            try:
-                img = Image.open(self.profile_picture.path)
-                
-                # Convert to RGB if necessary (for JPEG compatibility)
-                if img.mode != 'RGB':
-                    img = img.convert('RGB')
-                
-                # Resize to optimize storage (max 300x300 pixels)
-                max_size = (300, 300)
-                img.thumbnail(max_size, Image.Resampling.LANCZOS)
-                
-                # Save with optimization
-                img.save(self.profile_picture.path, 'JPEG', quality=85, optimize=True)
-            except Exception:
-                pass
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
