d2bd172552540337547f582c6cd4d3d89fb2031b
Roderick Sasu <38291077+Roderick47@users.noreply.github.com>
2026-07-03 16:01:03 +1000

    Add bulk upload success path to URLs

--- wikonomi/core/urls.py
+++ wikonomi/core/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
     path('how-to-use/', views.how_to_use_view, name='how_to_use'),
     path('api/map-prices/', views.api_map_prices, name='api_map_prices'),
     path('bulk-upload/', views.bulk_upload, name='bulk_upload'),
+    path('bulk-upload/success/', views.bulk_upload_success, name='bulk_upload_success'),
     path('bulk-upload/template/', views.download_csv_template, name='download_csv_template'),
 ]
