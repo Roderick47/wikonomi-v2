43cb30c43d6420e4f948a483408554a07c563ad3
Roderick Sasu <38291077+Roderick47@users.noreply.github.com>
2026-08-20 13:23:22 +1000

    Add welcome signup routes

--- wikonomi/users/urls.py
+++ wikonomi/users/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from . import signup_flow
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup'),
+    path('signup/', signup_flow.signup, name='signup'),
+    path('signup/complete/', signup_flow.signup_gateway, name='signup_gateway'),
+    path('welcome/', signup_flow.welcome, name='welcome'),
+    path('welcome/continue/', signup_flow.welcome_continue, name='welcome_continue'),
+    path('welcome/explore/', signup_flow.welcome_explore, name='welcome_explore'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
