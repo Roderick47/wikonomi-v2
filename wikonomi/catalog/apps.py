21e50a9e23070cce6e383fc6405e255c9a19173c
Roderick Sasu <38291077+Roderick47@users.noreply.github.com>
2026-09-08 10:12:11 +1000

    Activate product identity compatibility adapter

--- wikonomi/catalog/apps.py
+++ wikonomi/catalog/apps.py
@@ -5,3 +5,9 @@ class CatalogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'catalog'
     verbose_name = 'Product Catalog Identity'
+
+    def ready(self):
+        from .compat import install_core_product_matching_adapter
+        from . import signals  # noqa: F401
+
+        install_core_product_matching_adapter()
