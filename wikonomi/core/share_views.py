4e71e8dc47598f419b28c0b9b434cd6b592852f5
Roderick Sasu <38291077+Roderick47@users.noreply.github.com>
2026-09-11 07:35:54 +1000

    Add enriched price share landing page

--- wikonomi/core/share_views.py
+++ wikonomi/core/share_views.py
@@ -1,14 +1,34 @@
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
 from .models import PriceReport
 from .share_services import build_price_share_context
 
 
-def price_share_data(request, pk):
-    report = get_object_or_404(
-        PriceReport.objects.select_related('product', 'business', 'business_branch'),
+def _get_report(pk):
+    return get_object_or_404(
+        PriceReport.objects.select_related(
+            'product',
+            'business',
+            'business_branch',
+            'user',
+            'user__profile',
+        ).prefetch_related('photos'),
         pk=pk,
     )
+
+
+def price_share_page(request, pk):
+    report = _get_report(pk)
+    base_url = request.build_absolute_uri('/').rstrip('/')
+    share = build_price_share_context(report, base_url=base_url)
+    return render(request, 'price_share.html', {
+        'report': report,
+        'share': share,
+    })
+
+
+def price_share_data(request, pk):
+    report = _get_report(pk)
     base_url = request.build_absolute_uri('/').rstrip('/')
     return JsonResponse(build_price_share_context(report, base_url=base_url))
